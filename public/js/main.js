249bb08832336baae2c1ce9fd976a858b171a990
powerc9000 <clay.murray8@gmail.com>
2013-12-10 13:08:24 -0700

    some cool loading and status stuff
    returns many results instead of 1 and you can query on prety much anything now

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,48 @@
 angular.module("library", [])
-
+.directive("lazy", function(){
+	return {
+		link: function(scope, el, attrs){
+			el.bind("$destroy", function(){
+				lazy.remove(el[0]);
+			});
+			scope.$watch("books", function(){
+				lazy.register(el[0]);
+			})
+		}
+	}
+})
+.directive("coolLoader", function(){
+	return function(scope, el, attr){
+		var interval;
+		var count = 0;
+		scope.$watch(attr.ngShow, function(val){
+			if(val){
+				count = 0;
+				clearInterval(interval);
+				interval = setInterval(function(){
+					el.text("Loading"+new Array(count+1).join("."));
+					count = (count+1) % 4;
+				}, 200);
+			}else{
+				clearInterval(interval);
+			}
+		})
+	}
+})
 function lookup($scope, $http){
 	$scope.book = {};
+	$scope.ran = false;
+	$scope.loading = false;
 	$scope.lookupISBN = function(){
+		$scope.loading = true;
 		ISBN = ISBNLookupForm.ISBN.value;
-		$http.jsonp("https://www.googleapis.com/books/v1/volumes?q=isbn:"+ISBN+"&callback=JSON_CALLBACK").success(function(data){
+		$http.jsonp("https://www.googleapis.com/books/v1/volumes?q="+ISBN+"&callback=JSON_CALLBACK").success(function(data){
+			console.log(data);
+			$scope.loading = false;
+			$scope.ran = true;
 			console.log(typeof data);
-			$scope.book = data.items[0].volumeInfo;
+			$scope.totalItems = data.totalItems;
+			$scope.books = data.items;
 		})
 	}
 }
